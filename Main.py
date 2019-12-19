49ee41c27d80cdb95c857ce2cacb886e242f5b37
Lopez <OLIVIERB.LOPEZ@BNC.CA>
2019-12-19 16:06:37 -0500

    added date operation

--- Main.py
+++ Main.py
@@ -5,8 +5,8 @@ def main():
     cpte = Account("BNC", 500)
     print("Compte :", cpte.name)
 
-    ope  = Operation("DEP","EPICERIE",25)
-    ope.print_operation()
+    ope  = Operation("29-12-2019","DEP","EPICERIE",-25)
+    #ope.print_operation()
     cpte.add_operation(ope)
     bal = cpte.get_balance()
     print(bal)
--- Operation.py
+++ Operation.py
@@ -1,10 +1,11 @@
 class Operation:
     """represents an operation on an account"""
 
-    def __init__(self,optype,category,amount):
+    def __init__(self,date_ope,optype,category,amount):
         self.optype = optype
         self.category=category
         self.amount=amount
+        self.date_ope = date_ope
     
-    def print_operation(self):
-        print(self.optype,self.category,self.amount)
+    """ def print_operation(self):
+        print(self.date_ope,self.optype,self.category,self.amount) """
